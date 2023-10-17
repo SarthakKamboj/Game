74e598d6572befde227838a51b1c0459f9824462
SarthakKamboj <sarthakkamboj22@gmail.com>
2023-10-16 21:48:44 -0500

    reduce rigidbody struct size

--- src/physics/physics.h
+++ src/physics/physics.h
@@ -10,6 +10,8 @@ struct aabb_collider_t {
 	// for debugging
 	int collider_debug_render_handle;
 	int collider_debug_transform_handle;
+
+	// aabb_collider_t();
 };
 
 struct rigidbody_t {
@@ -18,20 +20,24 @@ struct rigidbody_t {
 	aabb_collider_t aabb_collider;
 	glm::vec2 vel = glm::vec2(0.f, 0.f);
 	int transform_handle = -1;
-	bool use_gravity = false;
-	bool is_kinematic = false;
-	bool debug = false;
+	bool use_gravity:1;
+	bool is_kinematic:1;
+	bool debug:1;
+
+	// rigidbody_t();
 };
 
-enum PHYSICS_COLLISION_DIR {
+enum PHYSICS_COLLISION_DIR: uint8_t {
 	NONE,
 	VERTICAL,
-	HORIZONTAL,
+	HORIZONTAL
 };
 
 struct collision_info_t {
-	bool is_colliding = false;
 	PHYSICS_COLLISION_DIR dir = PHYSICS_COLLISION_DIR::NONE;
+	bool is_colliding = false;
+
+	// collision_info_t();
 };
 
 int create_rigidbody(int transform_handle, bool use_gravity, float collider_width, float collider_height, bool is_kinematic);
