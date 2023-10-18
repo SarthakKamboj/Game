3bf52bc206e60d49b3f02553e50bece91eb67791
SarthakKamboj <sarthakkamboj22@gmail.com>
2023-10-18 02:25:13 -0500

    finished opengl buffer comments

--- src/renderer/opengl/buffers.h
+++ src/renderer/opengl/buffers.h
@@ -3,7 +3,7 @@
 #include "glad/glad.h"
 
 /// <summary>
-/// An OpenGL EBO
+/// An OpenGL Element Buffer Object
 /// </summary>
 struct ebo_t {
 	GLuint id = 0;
@@ -16,7 +16,7 @@ struct ebo_t {
 /// <param name="indicies">An array of indicies</param>
 /// <param name="size_of_buffer">The size of indicies in bytes (should be a multiple of the size of unsigned
 /// int on the system, which is most likely 4 bytes)</param>
-/// <returns></returns>
+/// <returns>Created EBO</returns>
 ebo_t create_ebo(const unsigned int* indicies, const int size_of_buffer);
 
 /// <summary>
@@ -27,7 +27,7 @@ ebo_t create_ebo(const unsigned int* indicies, const int size_of_buffer);
 /// int on the system, which is most likely 4 bytes)</param>
 /// <param name="usage_pattern">The usage pattern of the EBO. Particularly useful to declare when a EBO
 /// will be written to often.</param>
-/// <returns></returns>
+/// <returns>Created EBO</returns>
 ebo_t create_ebo(const unsigned int* indicies, const int size_of_buffer, GLenum usage_pattern);
 
 /// <summary>
@@ -43,7 +43,7 @@ void draw_ebo(const ebo_t& ebo);
 void bind_ebo(const ebo_t& ebo);
 
 /// <summary>
-/// Unbind an EBO to ensure changes aren't made to it anymore or finished drawing
+/// Unbind the current EBO to ensure changes aren't made to it anymore or finished drawing
 /// </summary>
 void unbind_ebo();
 
@@ -64,20 +64,74 @@ struct vbo_t {
 /// Creates an OpenGL VBO
 /// </summary>
 /// <param name="vertices">An array of the vertices</param>
-/// <param name="data_size"></param>
-/// <returns></returns>
+/// <param name="data_size">The size of the array of vertices in bytes</param>
+/// <returns>Create VBO</returns>
 vbo_t create_vbo(const float* vertices, const int data_size);
+
+/// <summary>
+/// Bind a particular VBO to OpenGL state
+/// </summary>
+/// <param name="vbo">VBO to bind</param>
 void bind_vbo(const vbo_t& vbo);
+
+/// <summary>
+/// Update the data associated with the VBO
+/// </summary>
+/// <param name="vbo">VBO to update</param>
+/// <param name="vertices">An array of the new vertex data</param>
+/// <param name="data_size">The size of the new vertex data array in bytes</param>
 void update_vbo_data(const vbo_t& vbo, const float* vertices, const int data_size);
+
+/// <summary>
+/// Unbinds the current VBO
+/// </summary>
 void unbind_vbo();
+
+/// <summary>
+/// Deletes a VBO
+/// </summary>
+/// <param name="vbo">VBO to delete</param>
 void delete_vbo(const vbo_t& vbo);
 
+/// <summary>
+/// OpenGL Vertex Array Object
+/// </summary>
 struct vao_t {
 	GLuint id = 0;
 };
 
+/// <summary>
+/// Creates a VAO
+/// </summary>
+/// <returns>Created VAO</returns>
 vao_t create_vao();
+
+/// <summary>
+/// Binds a VAO to current OpenGL state
+/// </summary>
+/// <param name="vao">VAO to bind</param>
 void bind_vao(const vao_t& vao);
+
+/// <summary>
+/// Unbinds current VAO
+/// </summary>
 void unbind_vao();
-void vao_enable_attribute(vao_t& vao, const vbo_t& vbo, const int attrId, const int numValues, const int dType, const int normalized, const int stride, const int offset);
+
+/// <summary>
+/// Binds some particular set of data (vertex attribute) from the VBO data buffer to the VAO to be used by OpenGL shaders later on.
+/// </summary>
+/// <param name="vao">VAO that will house the VBO's vertex attribute data</param>
+/// <param name="vbo">VBO with the data to be associated</param>
+/// <param name="attrId">The vertex attribute index to be modified. Basically the slot within the 
+/// VAO this VBO data will be stored.</param>
+/// <param name="numValues">Number of values associated with the VBO vertex attribute</param>
+/// <param name="dType">OpenGL type for the data to be stored</param>
+/// <param name="stride">Number of bytes between consecutive vertex attributes</param>
+/// <param name="offset">Offset in bytes of the first component of the first vertex attribute within the VBO buffer</param>
+void vao_enable_attribute(vao_t& vao, const vbo_t& vbo, const int attrId, const int numValues, const int dType, const int stride, const int offset);
+
+/// <summary>
+/// Deletes a VAO
+/// </summary>
+/// <param name="vao">VAO to delete</param>
 void delete_vao(const vao_t& vao);
