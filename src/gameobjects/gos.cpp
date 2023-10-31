2116670bc4cf0494cd541092b387f3aa93e7d489
SarthakKamboj <sarthakkamboj22@gmail.com>
2023-10-31 00:50:10 -0500

    Added a goomba object to the list of possible gameobjects

--- src/gameobjects/gos.cpp
+++ src/gameobjects/gos.cpp
@@ -2,6 +2,13 @@
 #include "constants.h" 
 #include <iostream>
 #include "utils/time.h"
+#include <vector>
+
+/**
+ * @brief Store all the goombas in the world and all the turning points
+*/
+std::vector<goomba_t> goombas;
+std::vector<goomba_turn_pt_t> goomba_turn_pts;
 
 main_character_t create_main_character(const glm::vec3& pos, const glm::vec3& scale, float rot, glm::vec3& color, const glm::vec2& dims) {
 	main_character_t mc;
@@ -13,24 +20,45 @@ main_character_t create_main_character(const glm::vec3& pos, const glm::vec3& sc
 
 void main_character_t::update(input::user_input_t& user_input) {
 
+	bool prev_dead = dead;
 	std::vector<general_collision_info_t>& col_infos = get_general_cols_for_non_kin_type(PHYSICS_RB_TYPE::PLAYER);
 	for (general_collision_info_t& col_info : col_infos) {
 		if (col_info.kin_type == PHYSICS_RB_TYPE::GROUND && col_info.rel_dir == PHYSICS_RELATIVE_DIR::BOTTOM) {
 			grounded = true;
 			num_jumps_since_grounded = 0;
 		}
+		if (col_info.kin_type == PHYSICS_RB_TYPE::GOOMBA) {
+			if (col_info.rel_dir == PHYSICS_RELATIVE_DIR::BOTTOM) {
+				delete_goomba_by_kin_handle(col_info.kin_handle);
+				// goombas.clear();
+				// rigidbody_t* kin_rb = get_rigidbody(col_info.kin_handle);
+				// assert(kin_rb);
+			}
+			else {
+				dead = true;
+			}
+		}
 	}
-
-    // get velocity
-	const float vel = WINDOW_WIDTH / 4.f;
 
     // get rigidbody and make sure its valid
     rigidbody_t* rb_ptr = get_rigidbody(rigidbody_handle);
     assert(rb_ptr != NULL);
 	rigidbody_t& rb = *rb_ptr;
 
+	if (!prev_dead && dead) {
+		// static float time_elapsed = 0;
+		rb.vel.y = WINDOW_HEIGHT / 2.f;
+		rb.detect_col = false;
+		return;
+	}
+
+	if (dead) return;
+
+    // get velocity
+	const float vel = WINDOW_WIDTH / 4.f;
+
     // jump
-    bool jump_btn_pressed = user_input.space_pressed;
+    bool jump_btn_pressed = user_input.w_pressed;
     bool character_falling = rb.vel.y <= 0;
 	if (jump_btn_pressed) {
 		if (grounded || num_jumps_since_grounded <= 1) {
@@ -55,7 +83,7 @@ void main_character_t::update(input::user_input_t& user_input) {
 
 }
 
-const glm::vec3 ground_block_t::BLOCK_COLOR = glm::vec3(0.58f, 0.29f, 0.f);
+const glm::vec3 ground_block_t::BLOCK_COLOR = glm::vec3(1.f, 0.29f, 0.f);
 int ground_block_t::tex_handle = -1;
 
 ground_block_t create_ground_block(const glm::vec3& pos, const glm::vec3& scale, float rot) {
@@ -63,7 +91,57 @@ ground_block_t create_ground_block(const glm::vec3& pos, const glm::vec3& scale,
 	block.transform_handle = create_transform(pos, scale, rot);
 	glm::vec3 color = ground_block_t::BLOCK_COLOR;
 	// block.rec_render_handle = create_rectangle_render(block.transform_handle, color, ground_block_t::WIDTH, ground_block_t::HEIGHT, false, 1.f, ground_block_t::tex_handle);
-	block.rec_render_handle = create_quad_render(block.transform_handle, color, ground_block_t::WIDTH, ground_block_t::HEIGHT, false, 1.f, ground_block_t::tex_handle);
+	block.rec_render_handle = create_quad_render(block.transform_handle, color, ground_block_t::WIDTH, ground_block_t::HEIGHT, false, 0.f, ground_block_t::tex_handle);
 	block.rigidbody_handle = create_rigidbody(block.transform_handle, false, ground_block_t::WIDTH, ground_block_t::HEIGHT, true, PHYSICS_RB_TYPE::GROUND);
 	return block;
 }
+
+const glm::vec3 goomba_t::GOOMBA_COLOR = glm::vec3(0.588f, 0.294f, 0.f);
+void create_goomba(const glm::vec3& pos) {
+	goomba_t goomba;
+	goomba.transform_handle = create_transform(pos, glm::vec3(1), 0.f);
+	glm::vec3 color = goomba_t::GOOMBA_COLOR;
+	goomba.rec_render_handle = create_quad_render(goomba.transform_handle, color, goomba_t::WIDTH, goomba_t::HEIGHT, false, 0.f, -1);
+	goomba.rigidbody_handle = create_rigidbody(goomba.transform_handle, false, goomba_t::WIDTH, goomba_t::HEIGHT, true, PHYSICS_RB_TYPE::GOOMBA);
+	goombas.push_back(goomba);
+}
+
+void update_goomba(goomba_t& goomba) {
+	transform_t* transform_ptr = get_transform(goomba.transform_handle);
+	assert(transform_ptr);
+
+	for (goomba_turn_pt_t& turn_pt : goomba_turn_pts) {
+		glm::vec2 diff_vec = glm::vec2(turn_pt.x, turn_pt.y) - glm::vec2(transform_ptr->position.x, transform_ptr->position.y);
+		if (glm::dot(diff_vec, diff_vec) <= 1.f) {
+			goomba.move_speed *= -1.f;
+		}
+	}
+
+	transform_ptr->position.x += goomba.move_speed * platformer::time_t::delta_time;
+}
+
+void delete_goomba_by_kin_handle(int kin_handle) {
+	int i_to_remove = -1;
+	for (goomba_t& goomba : goombas) {
+		i_to_remove++;
+		if (goomba.rigidbody_handle == kin_handle) {
+			delete_kin_rigidbody(kin_handle);
+			delete_quad_render(goomba.rec_render_handle);
+			delete_transform(goomba.transform_handle);
+			goombas.erase(goombas.begin() + i_to_remove);
+			return;
+		}
+	}
+}
+
+void add_goomba_turn_point(glm::vec3 pos) {
+	goomba_turn_pts.push_back(pos);
+}
+
+void gos_update() {
+	for (goomba_t& goomba : goombas) {
+		update_goomba(goomba);
+	}
+}
+
+
--- src/gameobjects/gos.h
+++ src/gameobjects/gos.h
@@ -4,6 +4,12 @@
 #include "transform/transform.h"
 #include "input/input.h"
 #include "physics/physics.h"
+#include "glm/glm.hpp"
+
+/**
+ * @brief Update all gameobjects
+*/
+void gos_update();
 
 struct main_character_t {
 	// create transform first
@@ -17,13 +23,12 @@ struct main_character_t {
 
 	bool grounded = false;
 	int num_jumps_since_grounded = 0;
+	bool dead = false;
 
 	void update(input::user_input_t& user_input);
 };
 
 main_character_t create_main_character(const glm::vec3& pos, const glm::vec3& scale, float rot, glm::vec3& color, const glm::vec2& dims);
-
-void update_main_character(const main_character_t& mc, input::user_input_t& user_input);
 
 struct ground_block_t {
 	// create transform first
@@ -40,3 +45,43 @@ struct ground_block_t {
 };
 
 ground_block_t create_ground_block(const glm::vec3& pos, const glm::vec3& scale, float rot);
+
+typedef glm::vec3 goomba_turn_pt_t;
+
+/**
+ * @brief Represents a Goomba
+*/
+struct goomba_t {
+	int transform_handle = -1;
+	int rec_render_handle = -1;
+	int rigidbody_handle = -1;
+	static const int WIDTH = 40;
+	static const int HEIGHT = 40;	
+	static const glm::vec3 GOOMBA_COLOR;
+	float move_speed = -50.f;
+};
+
+/**
+ * @brief Create a goomba at a particular position
+ * @param pos 
+*/
+void create_goomba(const glm::vec3& pos);
+
+/**
+ * @brief Update a Goomba
+ * @param goomba 
+*/
+void update_goomba(goomba_t& goomba);
+
+/**
+ * @brief Add a point in the world where goombas turn around to make sure
+ * they don't move through ground and walls
+ * @param pos Position of the turning point
+*/
+void add_goomba_turn_point(glm::vec3 pos);
+
+/**
+ * @brief Delete a goomba based on its rigidbody handle
+ * @param kin_handle Rigidbody handle of the goomba
+*/
+void delete_goomba_by_kin_handle(int kin_handle);
